be7f43c29f8f5f7e58af15bc253fe77e906fb22e
wilson <wsofoifa@gmail.com>
2026-05-09 18:43:19 -0500

    Develop 02 Finished

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,47 @@
+using System.IO; 
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string file)
+    {
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string file)
+    {
+        _entries.Clear(); // Requirements state loading should replace current entries
+        string[] lines = File.ReadAllLines(file);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+            Entry loadedEntry = new Entry
+            {
+                _date = parts[0],
+                _promptText = parts[1],
+                _entryText = parts[2]
+            };
+            _entries.Add(loadedEntry);
+        }
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,11 +1,35 @@
-using System;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        int height;
-        string color;
-        
+        Journal theJournal = new Journal();
+        PromptQuestion promptQuestion = new PromptQuestion();
+        string choice = "";
+        while (choice != "5")
+        {
+            Console.WriteLine("Please select a choice:");
+            Console.WriteLine("1. Write\n2. Display\n3. Load\n4 Save\n5. Quit");
+            Console.Write("What would you like to do?");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptQuestion.GetRandomQuestion();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string response = Console.ReadLine();
+                string date = DateTime.Now.ToShortDateString();
+
+                Entry newEntry = new Entry { _date = date, _promptText = prompt, _entryText = response }; 
+                theJournal.AddEntry(newEntry);
+            }
+            else if (choice == "2")
+                {
+                    theJournal.DisplayAll();
+                }
+        }
     }
+   
 }
